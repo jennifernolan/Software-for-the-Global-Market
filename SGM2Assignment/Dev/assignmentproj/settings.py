ec60c6fb0daf0d8f80032e5cbc08a61681f82fc4
jennifernolan <j-nolan77@hotmail.com>
2018-04-14 18:23:23 +0100

    localization

--- SGM2Assignment/Dev/assignmentproj/settings.py
+++ SGM2Assignment/Dev/assignmentproj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'django.contrib.humanize',
 	'assignmentapp',
 	'accounts',
 ]
@@ -137,7 +138,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'static'),
 ]
 
